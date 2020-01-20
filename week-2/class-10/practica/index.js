fa84b4a2e47978ae6d55ea95e6abf5aacd1c79ee
Moisés Fernández <moy-881@hotmail.com>
2020-01-20 07:13:20 -0600

    Class 10 practice

--- week-2/class-10/practica/index.js
+++ week-2/class-10/practica/index.js
@@ -1,45 +1,64 @@
-function fetchDog() {
+function fetchDog2(){
     let url = "https://dog.ceo/api/breeds/image/random";
-    let settings = {
-        method : "GET"
-    };
 
-    fetch(url, settings)
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-            }
-
-            throw new Error(response.statusText);
-        })
-        .then((responseJSON) => {
-            console.log(responseJSON);
-        })
-}
-
-function displayResults(responseJSON) {
-    let results = document.getElementsByClassName('results');
-
-    results[0].innerHTML = `
-        <h1> Your random image </h1>
-        <p>
-            <img src="${responseJSON.message}" />
-        </p>
-    `
-}
-
-function watchForm() {
-    let form = document.getElementById('randomDog');
-
-    form.addEventListener('submit', (event) => {
-        event.preventDefault(); // Cuando se manejan formas, para que la formada no sea refreshed.
-
-        
+    $.ajax({
+      url : url,
+      method : "GET",
+      dataType : "json",
+      success : function( responseJSON ){
+        displayResults( responseJSON );
+      },
+      error : function( err ){
+        console.log( err );
+      }
     });
 }
 
-function init() {
-    watchForm();
+function fetchDog(){
+
+  let url = "https://dog.ceo/api/breeds/image/random";
+  let settings = {
+    method : "GET"
+  };
+  
+  fetch( url, settings )
+    .then( (response) => {
+      if ( response.ok ){
+        return response.json();
+      }
+
+      throw new Error( response.statusText );
+    })
+    .then( (responseJSON) => {
+      displayResults( responseJSON );
+    });
+}
+
+function displayResults( responseJSON ){
+  let results = document.getElementsByClassName( 'results' );
+
+  results[0].innerHTML = `
+    <h1> Your random image </h1>
+    <p>
+      <img class="dimension" src="${responseJSON.message}" />
+    </p>
+  `;
+
+}
+
+function watchForm(){
+  
+  let form = document.getElementById( 'randomDog' );
+
+  form.addEventListener( 'submit' , ( event ) => {
+    event.preventDefault();
+
+    fetchDog2();
+  });
+}
+
+function init(){
+  watchForm();
 }
 
 init();
